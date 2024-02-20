36c1197c69d03685936b46684fa505b5cbffe353
chelluboinajyothiswarup <100918884+chelluboinajyothiswarup@users.noreply.github.com>
2024-02-20 15:15:12 +0530

    Create 1661. Average Time of Process per Machine.sql

---
+++ 1661. Average Time of Process per Machine.sql
@@ -0,0 +1,25 @@
+-- APPROACH 1
+SELECT 
+    machine_id,
+    ROUND(SUM(CASE WHEN activity_type='start' THEN timestamp*-1 ELSE timestamp END)*1.0
+    / (SELECT COUNT(DISTINCT process_id)),3) AS processing_time
+FROM 
+    Activity
+GROUP BY machine_id
+
+
+-- APPROACH 2
+
+SELECT a.machine_id, 
+       ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
+FROM Activity a, 
+     Activity b
+WHERE 
+    a.machine_id = b.machine_id
+AND 
+    a.process_id = b.process_id
+AND 
+    a.activity_type = 'start'
+AND 
+    b.activity_type = 'end'
+GROUP BY machine_id
